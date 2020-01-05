4e5928bdf545610f191a038f8562ecaaba32dbcf
Leejeeyong <fghj0720@gmail.com>
2020-01-05 16:36:38 +0900

    retrofit documents tutorial  project-3

--- restapitest0012/app/src/main/java/com/example/restapitest001/MainActivity.java
+++ restapitest0012/app/src/main/java/com/example/restapitest001/MainActivity.java
@@ -1,9 +1,17 @@
 package com.example.restapitest001;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
 
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -14,10 +22,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        String carriername = "kr.epost"; //택배사 이름
+        String tracknumberedit = "1111111111"; //송장번
+
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://apis.tracker.delivery")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         final Retroservice retroService = retrofit.create(Retroservice.class);
+
+        Call<Data> dataP = retroService.trackingData(carriername, tracknumberedit);
+
+        dataP.enqueue(new Callback<Data>() {
+            @Override
+            public void onResponse(@NonNull Call<Data> call, @NonNull Response<Data> response) {
+
+                Data user = response.body();
+
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<Data> call, @NonNull Throwable t) {
+                Log.d("TESt", "Failure");
+            }
+        });
     }
 }
